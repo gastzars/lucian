f1a912368ce147bc0bb651e7fe2de7ecc3977dee
Gas unknown developer <gas@gas.com>
2016-11-02 18:17:50 +0700

    suppress rspec puts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,27 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "lucian"
+
+RSpec.configure do |config|
+  config.before(:all, &:silence_output)
+  config.after(:all,  &:enable_output)
+end
+
+public
+# Redirects stderr and stout to /dev/null.txt
+def silence_output
+  # Store the original stderr and stdout in order to restore them later
+  @original_stderr = $stderr
+  @original_stdout = $stdout
+
+  # Redirect stderr and stdout
+  $stderr = File.new(File.join(File.dirname(__FILE__), 'dev', 'null.txt'), 'w')
+  $stdout = File.new(File.join(File.dirname(__FILE__), 'dev', 'null.txt'), 'w')
+end
+
+# Replace stderr and stdout so anything else is output correctly
+def enable_output
+  $stderr = @original_stderr
+  $stdout = @original_stdout
+  @original_stderr = nil
+  @original_stdout = nil
+end
